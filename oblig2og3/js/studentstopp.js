cdfb2532f129a57866f45aa905cc170ae7fe3a89
Celina <celinakristiansen@hotmail.com>
2016-10-12 11:35:07 +0200

    Update studentstopp.js

--- oblig2og3/js/studentstopp.js
+++ oblig2og3/js/studentstopp.js
@@ -1,1 +1,45 @@
 
+    window.onload = oppstart;
+
+      function oppstart () {
+
+        var reg = document.getElementById("btnReg").onclick = registrer;
+        // Lager Knapp for rgistrering + variabel
+      }
+
+      function registrer () { // Funksjon for knappen
+        var bruker = document.getElementById("txtBrukerNavn").value; // Brukernavn
+        var passord = document.getElementById("txtPassord").value; // Passord
+        var bekreft = document.getElementById("txtBekreft").value; // Bekreft passord
+        var alder = document.getElementById("txtDato").value; // Alder
+        var min = 5; // Dette er en variabel for mer en 5 "karakterer"
+
+        var svar = ""; // Variabel for hva som printes ut
+
+
+        if (bruker === "") { // Hvis brukernavn boxen er tom, print dette "Fyll inn..."
+          svar = "Fyll inn brukernavn";
+        }
+        else if (passord === "") { // Hvis passords boxen er tom, print ....
+          svar = "Fyll inn passord";
+        }
+        else if (passord.length <= min) // Hvis passordet også er mindre en 5 "karaktere"
+                                       // print dette...
+          svar = "Passord for kort";
+
+        else if (bekreft === "") { // Hvis bekreft passord er tomt, print dette...
+          svar = "Bekreft passord";
+        }
+        else if (passord !== bekreft) { // Bekreft passord ikke lik passordet, print dette...
+          svar = "Passord samsvarer ikke"; // Osv...
+        }
+        else if (alder === "") {
+          svar = "Fyll ut alder";
+        }
+        else if (alder < 18)
+          svar = "Du er ikke gammel nok!" // Om aldere som er tastet inn, er minder en 18, print...
+        else {
+          svar = "Takk for registrering";
+        }
+        document.getElementById("print").innerHTML = svar; // Skrives ut i <p>
+    }
